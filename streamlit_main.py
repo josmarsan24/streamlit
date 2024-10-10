3947a6c8955751dddd149bb2bb421b1acb3f0eaa
josmarsan24 <josmarsan24@alum.us.es>
2024-10-10 18:57:31 +0200

    add url main page

--- streamlit_main.py
+++ streamlit_main.py
@@ -10,7 +10,7 @@ st.subheader("Demo app by Jose Martin")
 url = 'https://www.basketball-reference.com/leagues/NBA_2024_per_game.html'
 urlComp = '/compare'
 urlPlot = '/plot'
-urlGit = ''
+urlGit = 'https://github.com/josmarsan24/streamlit'
 
 st.markdown("This app was made to explore the players data from the 2023-2024 season, this was made with streamlit, the data source is " f"[BasketballReference]({url})")
 st.markdown("You can compare two players data in the " f"[compare players]({urlComp})" " tab and plot different statistics in the " f"[plot metrics]({urlPlot})" " tab")
